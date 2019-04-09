1ad4fe9ccefdf5dcc1166d7fa8b116f3f126f3e2
vicma <vic.maint@outlook.fr>
2019-04-09 17:26:31 +0200

    commit fin de cours story4 début

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -65,4 +65,9 @@ public void positionnerUnNouveauVaisseau(int x, int y) {
 		if (vaisseau.abscisse()< (longueur-1)) vaisseau.seDeplacerVersLaDroite();
 		
 	}
+
+	public void deplacerVaisseauVersLaGauche() {
+		if (vaisseau.abscisse()> 0) vaisseau.seDeplacerVersLaGauche();
+		
+	}
 }
